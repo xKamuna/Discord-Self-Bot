f0ea54004aa43df68edb9de25ed5ef6e6187d583
Favna <jeroen.claassens@live.nl>
2017-05-14 00:49:42 +0200

    re-enable log events

--- src/app.js
+++ src/app.js
@@ -23,40 +23,40 @@ client
 			Prefix ${prefix === '' ? 'removed' : `changed to ${prefix || 'the default'}`}
 			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
 		`);
+    })
+    .on('error', console.error)
+    .on('warn', console.warn)
+    .on('debug', console.log)
+    .on('disconnect', () => {
+        console.warn('Disconnected!');
+    })
+    .on('reconnecting', () => {
+        console.warn('Reconnecting...');
+    })
+    .on('commandError', (cmd, err) => {
+        if (err instanceof Commando.FriendlyError) return;
+        console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
+    })
+    .on('commandBlocked', (msg, reason) => {
+        console.log(oneLine `
+		Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ''}
+		blocked; ${reason}
+	`);
+    })
+    .on('commandStatusChange', (guild, command, enabled) => {
+        console.log(oneLine `
+		Command ${command.groupID}:${command.memberName}
+		${enabled ? 'enabled' : 'disabled'}
+		${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+	`);
+    })
+    .on('groupStatusChange', (guild, group, enabled) => {
+        console.log(oneLine `
+		Group ${group.id}
+		${enabled ? 'enabled' : 'disabled'}
+		${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+	`);
     });
-// .on('error', console.error)
-// .on('warn', console.warn)
-// .on('debug', console.log)
-// .on('disconnect', () => {
-//     console.warn('Disconnected!');
-// })
-// .on('reconnecting', () => {
-//     console.warn('Reconnecting...');
-// })
-// .on('commandError', (cmd, err) => {
-//     if (err instanceof Commando.FriendlyError) return;
-//     console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
-// })
-// .on('commandBlocked', (msg, reason) => {
-//     console.log(oneLine `
-// 		Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ''}
-// 		blocked; ${reason}
-// 	`);
-// })
-// .on('commandStatusChange', (guild, command, enabled) => {
-//     console.log(oneLine `
-// 		Command ${command.groupID}:${command.memberName}
-// 		${enabled ? 'enabled' : 'disabled'}
-// 		${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
-// 	`);
-// })
-// .on('groupStatusChange', (guild, group, enabled) => {
-//     console.log(oneLine `
-// 		Group ${group.id}
-// 		${enabled ? 'enabled' : 'disabled'}
-// 		${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
-// 	`);
-// });
 
 
 client.setProvider(

51c908d71f44ac729ae515bc1e492758f879d6cd
Favna <jeroen.claassens@live.nl>
2017-05-18 13:47:23 +0200

    fix a dormant error that occured when talking in DM

--- src/app.js
+++ src/app.js
@@ -91,7 +91,7 @@ client.on("message", msg => {
         const content = msg.content.toLowerCase();
         const args = msg.content.split(' ').slice(1);
         const storeChannel = client.channels.get(messageStoreChannelID);
-        let prefix = msg.guild.commandPrefix;
+        let prefix = msg.guild.commandPrefix !== null ? msg.guild.commandPrefix : '$'
 
         let currentStoreSize = messageStore.length;
         if (currentStoreSize === 10) {

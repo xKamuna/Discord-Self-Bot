71e918193475845ed0ed17fb84aab9a1d8a6820d
Favna <jeroen.claassens@live.nl>
2017-09-13 11:08:46 +0200

    Update license headers to reflect name change [skip ci]

--- src/commands/misc/zalgo.js
+++ src/commands/misc/zalgo.js
@@ -1,19 +1,19 @@
 // Copyright (C) 2017 Favna
 // 
-// This file is part of PyrrhaBot.
+// This file is part of Discord-Self-Bot.
 // 
-// PyrrhaBot is free software: you can redistribute it and/or modify
+// Discord-Self-Bot is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 // 
-// PyrrhaBot is distributed in the hope that it will be useful,
+// Discord-Self-Bot is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 // 
 // You should have received a copy of the GNU General Public License
-// along with PyrrhaBot.  If not, see <http://www.gnu.org/licenses/>.
+// along with Discord-Self-Bot.  If not, see <http://www.gnu.org/licenses/>.
 // 
 
 const zalgo = require('zalgotxt');

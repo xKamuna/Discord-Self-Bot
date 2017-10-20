1c196e5c46991543b3bbd5c63d93e8079af85cb4
Favna <jeroen.claassens@live.nl>
2017-10-20 23:39:08 +0200

    input discriminator

--- src/commands/info/fyidm.js
+++ src/commands/info/fyidm.js
@@ -1,3 +1,21 @@
+// Copyright (C) 2017 Favna
+// 
+// This file is part of Discord-Self-Bot.
+// 
+// Discord-Self-Bot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Discord-Self-Bot is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Discord-Self-Bot.  If not, see <http://www.gnu.org/licenses/>.
+// 
+
 const Discord = require("discord.js");
 const commando = require('discord.js-commando');
 const moment = require('moment');
@@ -11,14 +29,22 @@ module.exports = class fyidmCommand extends commando.Command {
             memberName: 'fyidm',
             description: 'Returns a list of users who have the same discriminator (the 4 digits after the username) as you. fyidm is short for "Find your ID Mate"',
             examples: ['fyidm'],
-            guildOnly: false
+            guildOnly: false,
+
+            args: [{
+                key: 'discrim',
+                prompt: 'Check which discriminator',
+                type: 'string',
+                default: 'self'
+            }]
         });
     }
 
     async run(msg, args) {
         const fyidmEmbed = new Discord.RichEmbed();
+        const discrim = args.discrim === 'self' ? msg.author.discriminator : args.discrim;
 
-        var discrimMatches = msg.client.users.filter(u => u.discriminator === msg.author.discriminator);
+        var discrimMatches = msg.client.users.filter(u => u.discriminator === discrim);
         discrimMatches.delete(msg.author.id);
         var matchEntries = discrimMatches.entries()
 

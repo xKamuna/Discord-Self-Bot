826292bc3257c1eba7f36945216d2d7d246c4600
Jeroen Claassens <jeroen.claassens@live.nl>
2018-05-19 03:58:41 +0200

    remove momentFormat
    I've moved Ribbon  to just use setTimestamp so it always properly reflects a relative date compared to the viewers timezone which is much more worldwide friendly

--- src/util.js
+++ src/util.js
@@ -10,12 +10,6 @@ const capitalizeFirstLetter = function (string) {
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 };
 
-const momentFormat = function (date, client) {
-  const moment = require('moment'); // eslint-disable-line global-require
-
-  return moment(date).format(`MMMM Do YYYY [at] ${client.provider.get('global', 'timeformat', '24') === '24' ? 'HH:mm:ss' : 'hh:mm:ss A'} [UTC]Z`);
-};
-
 const stopTyping = function (msg) {
   msg.channel.stopTyping(true);
 };
@@ -27,7 +21,6 @@ const startTyping = function (msg) {
 module.exports = {
   capitalizeFirstLetter,
   deleteCommandMessages,
-  momentFormat,
   stopTyping,
   startTyping
 };

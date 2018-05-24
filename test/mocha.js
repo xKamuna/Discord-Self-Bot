3e88b70a693f5068b5e7302c2a7ae0304d3e86bc
Jeroen Claassens <jeroen.claassens@live.nl>
2018-05-24 03:02:21 +0200

    add docs and tests

---
+++ test/mocha.js
@@ -0,0 +1,74 @@
+/**
+ * @file Discord-Self-Bot Tests - Test Discord-Self-Bot with Mocha
+ * @author Jeroen Claassens (favna) <sharkie.jeroen@gmail.com>
+ * @copyright © 2017-2018 Favna  
+ *  
+ *   This file is part of discord-self-bot
+ *   Copyright (C) 2017-2018 Favna
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, version 3 of the License
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* eslint-disable no-undef, no-unused-vars, sort-vars, no-mixed-requires, global-require*/
+
+const Database = require('better-sqlite3'),
+  assert = require('assert'),
+  should = require('chai').should(),
+  path = require('path'),
+  {expect} = require('chai'),
+  {Client, SyncSQLiteProvider} = require('discord.js-commando');
+
+beforeEach('Injecting dotenv', () => {
+  require('dotenv').config({path: path.join(__dirname, '../src/.env')});
+});
+
+describe('Check dotenv', () => {
+  it('token should be set', () => {
+    const token = process.env.testtoken;
+
+    expect(token).to.be.ok;
+  });
+  it('google api token should be set', () => {
+    const token = process.env.googleapikey;
+
+    expect(token).to.be.ok;
+  });
+});
+
+describe('Connect & Disconnect bot', () => {
+  it('should connect then disconnect', () => {
+    const client = new Client({
+        commandPrefix: 's!!s',
+        owner: process.env.owner,
+        selfbot: true,
+        unknownCommandResponse: false
+      }),
+      db = new Database(path.join(__dirname, '../src/data/databases/settings.sqlite3'));
+
+    client.setProvider(
+      new SyncSQLiteProvider(db)
+    );
+    let readyTracker = false;
+
+    client.registry.registerDefaults();
+    client.login(process.env.testtoken);
+
+    client.on('ready', () => {
+      console.log('im ready!');
+      readyTracker = true;
+      client.destroy();
+      process.exit(); // eslint-disable-line no-process-exit
+      expect(readyTracker).to.be.ok;
+    });
+  });
+});

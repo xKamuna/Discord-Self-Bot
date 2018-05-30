08f9671796e4135eedec96f545633cf78dc2f6c9
Jeroen Claassens <jeroen.claassens@live.nl>
2018-05-30 19:50:02 +0200

    Create webpage for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,28 @@ require('dotenv').config({path: path.join(__dirname, '.env')});
 const DiscordSelfBot = require(path.join(__dirname, 'DiscordSelfBot.js')),
   start = function () {
     new DiscordSelfBot(process.env.token).init();
+  },
+  heroku = function () {
+    /* eslint-disable */
+    const express = require('express');
+    const app = express();
+    const PORT = 8080;
+
+    app
+      .set('view engine', 'html')
+      .use(express.static(path.join(__dirname, 'heroku')))
+      .all('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'heroku/heroku.html'));
+      })
+      .listen(PORT, () => {
+        console.log(`Server started on http://localhost:${PORT}`);
+      });
+      /* eslint-enable */
   };
 
-start();
+if (process.env.heroku) {
+  heroku();
+  start();
+} else {
+  start();
+}
